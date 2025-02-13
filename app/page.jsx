507e8e2be59981808a41828a59b337613fc9f6e4
Sharique Imam <shariquegithubweb@gmail.com>
2025-02-13 14:55:50 +0500

    Test

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,15 @@
+"use client";
 import Footer2 from "@/components/footers/Footer2";
 import Header10 from "@/components/headers/Header10";
 import Home1 from "@/components/homes/home-1";
 import { strongMultiPages } from "@/data/menu";
 import Hero4 from "@/components/homes/home-1/heros/Hero4";
 
-export const metadata = {
-  title: "Home Care Management || Compassionate Home Care Services",
-  description:
-    "Providing high-quality, compassionate home care services tailored to support independence, dignity, and well-being. ",
-};
+// export const metadata = {
+//   title: "Home Care Management || Compassionate Home Care Services",
+//   description:
+//     "Providing high-quality, compassionate home care services tailored to support independence, dignity, and well-being. ",
+// };
 export default function Home1SliderBackgroundMultiPage() {
   return (
     <>
